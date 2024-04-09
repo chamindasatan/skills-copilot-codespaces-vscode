28a587e5e27b584d1395ff7c2e35132956b2d503
chaminda satanarachchi <chamin.7896@gmail.com>
2024-04-09 12:37:41 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,15 @@
+//create web server
+const express = require('express');
+const app = express();
+const port = 3000;
+
+//create a route
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+//start the server
+
+app.listen(port, () => {
+}); // Add a closing curly brace here
+    
